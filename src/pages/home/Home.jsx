82b2b8ec89e3e773ae94fd6ff1bf6fd8a1ca0963
Sergey87 <serge.techno@gmail.com>
2023-11-02 21:36:47 +0300

    added navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Home.css';
+import Navbar from '../../components/navbar/Navbar';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
