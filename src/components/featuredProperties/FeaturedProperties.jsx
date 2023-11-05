f3b9b8dd87605f3319a15b2d5f5da06071046dab
Sergey87 <serge.techno@gmail.com>
2023-11-05 17:05:50 +0300

    added email list component

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -11,11 +11,12 @@ const FeaturedProperties = () => {
           alt='fp pic'
         />
         <span className='fpName'>Aparthotel Stare Miasto</span>
-        <span className='fpCity'>Madrid</span>
-        <span className='fpPrice'>Starting from 120$</span>
-        <div className='fpRating'></div>
-        <button>8.9</button>
-        <span>Excellent</span>
+        <span className='fpCity'>London</span>
+        <span className='fpPrice'>Starting from 150$</span>
+        <div className='fpRating'>
+          <button>9.2</button>
+          <span>Excellent</span>
+        </div>
       </div>
       <div className='fpItem'>
         <img
@@ -24,37 +25,40 @@ const FeaturedProperties = () => {
           alt='fp pic'
         />
         <span className='fpName'>Aparthotel Stare Miasto</span>
-        <span className='fpCity'>Madrid</span>
+        <span className='fpCity'>Berlin</span>
         <span className='fpPrice'>Starting from 120$</span>
-        <div className='fpRating'></div>
-        <button>8.9</button>
-        <span>Excellent</span>
+        <div className='fpRating'>
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
       </div>
       <div className='fpItem'>
         <img
           className='fpImg'
-          src='https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+          src='https://images.pexels.com/photos/1579253/pexels-photo-1579253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
           alt='fp pic'
         />
         <span className='fpName'>Aparthotel Stare Miasto</span>
-        <span className='fpCity'>Madrid</span>
-        <span className='fpPrice'>Starting from 120$</span>
-        <div className='fpRating'></div>
-        <button>8.9</button>
-        <span>Excellent</span>
+        <span className='fpCity'>Paris</span>
+        <span className='fpPrice'>Starting from 100$</span>
+        <div className='fpRating'>
+          <button>8.5</button>
+          <span>Excellent</span>
+        </div>
       </div>
       <div className='fpItem'>
         <img
           className='fpImg'
-          src='https://thumbs.dreamstime.com/b/hotel-bed-28403860.jpg'
+          src='https://cdn.pixabay.com/photo/2016/10/18/09/02/hotel-1749602_640.jpg'
           alt='fp pic'
         />
         <span className='fpName'>Aparthotel Stare Miasto</span>
         <span className='fpCity'>Madrid</span>
-        <span className='fpPrice'>Starting from 120$</span>
-        <div className='fpRating'></div>
-        <button>8.9</button>
-        <span>Excellent</span>
+        <span className='fpPrice'>Starting from 170$</span>
+        <div className='fpRating'>
+          <button>9.1</button>
+          <span>Excellent</span>
+        </div>
       </div>
     </div>
   );
