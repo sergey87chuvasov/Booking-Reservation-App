a062cb793ae1b92825da6bd5d4cc37a74da83983
Sergey87 <serge.techno@gmail.com>
2023-11-06 20:24:30 +0300

    added Search Result List Page - 1 part

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBed,
@@ -16,6 +17,7 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import format from 'date-fns/format';
 
 const Header = ({ type }) => {
+  const [destination, setDestination] = useState('');
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
@@ -32,6 +34,8 @@ const Header = ({ type }) => {
     room: 1,
   });
 
+  const navigate = useNavigate();
+
   const handleOption = (name, operation) => {
     setOptions((prev) => {
       return {
@@ -39,6 +43,10 @@ const Header = ({ type }) => {
         [name]: operation === 'i' ? options[name] + 1 : options[name] - 1,
       };
     });
+  };
+
+  const handleSearch = () => {
+    navigate('/hotels', { state: { destination, date, options } });
   };
 
   return (
@@ -89,6 +97,7 @@ const Header = ({ type }) => {
                   type='text'
                   placeholder='Where are you going?'
                   className='headerSearchInput'
+                  onChange={(e) => setDestination(e.target.value)}
                 />
               </div>
               <div className='headerSearchItem'>
@@ -187,7 +196,9 @@ const Header = ({ type }) => {
                 )}
               </div>
               <div className='headerSearchItem'>
-                <button className='headerBtn'>Search</button>
+                <button className='headerBtn' onClick={handleSearch}>
+                  Search
+                </button>
               </div>
             </div>
           </>
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import './List.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import { useLocation } from 'react-router-dom';
 
 const List = () => {
+  const location = useLocation();
+
+  console.log(location);
   return (
     <div>
       <Navbar />
       <Header type='list' />
+      <div className='listContainer'>
+        <div className='listWrapper'>
+          <div className='listSearch'>
+            <h1 className='lsTitle'>Search</h1>
+            <div className='lsItem'>
+              <label>Destination</label>
+              <input type='text' />
+            </div>
+            <div className='lsItem'>
+              <label>Check-in-Date</label>
+            </div>
+          </div>
+          <div className='listResult'></div>
+        </div>
+      </div>
     </div>
   );
 };
