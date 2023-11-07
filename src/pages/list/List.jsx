01b8a60e64bcc3cadd8ef28c367520b48b892ef4
Sergey87 <serge.techno@gmail.com>
2023-11-07 16:52:38 +0300

    added Search Result List Page - part 2

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -3,11 +3,18 @@ import './List.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import format from 'date-fns/format';
+import { DateRange } from 'react-date-range';
 
 const List = () => {
   const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
 
-  console.log(location);
+  // console.log(location);
   return (
     <div>
       <Navbar />
@@ -18,10 +25,24 @@ const List = () => {
             <h1 className='lsTitle'>Search</h1>
             <div className='lsItem'>
               <label>Destination</label>
-              <input type='text' />
+              <input type='text' placeholder={destination} />
             </div>
             <div className='lsItem'>
               <label>Check-in-Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                'MM/dd/yyyy'
+              )} to ${format(date[0].endDate, 'MM/dd/yyyy')}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+            </div>
+            <div className='lsItem'>
+              <label>Options</label>
             </div>
           </div>
           <div className='listResult'></div>
